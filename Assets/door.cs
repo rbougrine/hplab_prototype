0663685bdb0ac1183b569905fe738aba1cd94f17
randa <rrbougrine@gmail.com>
2016-03-28 22:26:55 +0200

    change of repository, added code for moving door. Bedroom completed.

---
+++ Assets/door.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class door : MonoBehaviour
+{
+    public bool doorisClosed;
+    public Animator anim;
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "First Person Controller")
+        {
+
+             if (doorisClosed == true)
+              {
+                anim.Play("openDoor");
+                doorisClosed = false;
+                yield return new WaitForSeconds(10);
+                anim.Play("closeDoor");
+                doorisClosed = true;
+              }
+            
+            }
+        }
+    }
+ 
