ef88c5e8a39285de32d47c264338efa5fb693dfb
AnnyA95 <anni032@hotmail.com>
2016-04-03 12:32:06 +0200

    Final prototype

---
+++ Assets/code/door.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door : MonoBehaviour
+{
+    public bool doorisClosed;
+    public Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+       
+    }
+
+    public void doorAction()
+    {
+   
+        StartCoroutine(doorMovement());
+
+
+    }
+    IEnumerator doorMovement()
+    {
+        if (doorisClosed == true)
+        {
+            anim.Play("openDoor");
+            doorisClosed = false;
+            yield return new WaitForSeconds(5);
+            anim.Play("closeDoor");
+            doorisClosed = true;
+        }
+
+    }
+}   
+ 
